c35b983f09d1ca1e567eb7ebfc98cb1746ac14cf
sategie <sategie@yahoo.com>
2023-04-10 21:17:00 +0000

    Create initial set of Q&A array

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+// Create Q&A array
+
+let questions = [
+    {
+        question: "How many continents are there in the world?",
+        answers: [
+            {text:"5", correct:false},
+            {text:"7", correct:true},
+            {text:"8", correct:false},
+            {text:"9", correct:false},
+        ]
+        
+    },
+    {
+        question: "How many continents are there in the world?",
+        answers: [
+            {text:"5", correct:false},
+            {text:"7", correct:true},
+            {text:"8", correct:false},
+            {text:"9", correct:false},
+        ]
+        
+    },
+    {
+        question: "How many continents are there in the world?",
+        answers: [
+            {text:"5", correct:false},
+            {text:"7", correct:true},
+            {text:"8", correct:false},
+            {text:"9", correct:false},
+        ]
+        
+    },
+    {
+        question: "How many continents are there in the world?",
+        answers: [
+            {text:"5", correct:false},
+            {text:"7", correct:true},
+            {text:"8", correct:false},
+            {text:"9", correct:false},
+        ]
+        
+    },
+    {
+        question: "How many continents are there in the world?",
+        answers: [
+            {text:"5", correct:false},
+            {text:"7", correct:true},
+            {text:"8", correct:false},
+            {text:"9", correct:false},
+        ]
+        
+    }
+
+];
+
