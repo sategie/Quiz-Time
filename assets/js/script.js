f1f52c138acd2263d4c9048c4188b176cd4af1c2
sategie <sategie@yahoo.com>
2023-04-11 09:51:29 +0000

    Update Q&A array

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,47 +7,47 @@ let questions = [
             {text:"5", correct:false},
             {text:"7", correct:true},
             {text:"8", correct:false},
-            {text:"9", correct:false},
+            {text:"9", correct:false}
         ]
         
     },
     {
-        question: "How many continents are there in the world?",
+        question: "What is the capital of Nigeria?",
         answers: [
-            {text:"5", correct:false},
-            {text:"7", correct:true},
-            {text:"8", correct:false},
-            {text:"9", correct:false},
+            {text:"Lagos", correct:false},
+            {text:"Ibadan", correct:false},
+            {text:"Abuja", correct:true},
+            {text:"Port-Harcourt", correct:false}
         ]
         
     },
     {
-        question: "How many continents are there in the world?",
+        question: "How many outfield players are there in football/soccer?",
         answers: [
-            {text:"5", correct:false},
-            {text:"7", correct:true},
+            {text:"11", correct:false},
+            {text:"10", correct:true},
             {text:"8", correct:false},
-            {text:"9", correct:false},
+            {text:"9", correct:false}
         ]
         
     },
     {
-        question: "How many continents are there in the world?",
+        question: "How many states are there in Germany?",
         answers: [
-            {text:"5", correct:false},
-            {text:"7", correct:true},
+            {text:"16", correct:true},
+            {text:"7", correct:false},
             {text:"8", correct:false},
-            {text:"9", correct:false},
+            {text:"9", correct:false}
         ]
         
     },
     {
-        question: "How many continents are there in the world?",
+        question: "How many milliseconds are there in a second?",
         answers: [
-            {text:"5", correct:false},
-            {text:"7", correct:true},
-            {text:"8", correct:false},
-            {text:"9", correct:false},
+            {text:"24", correct:false},
+            {text:"60", correct:false},
+            {text:"100", correct:false},
+            {text:"1000", correct:true}
         ]
         
     }
