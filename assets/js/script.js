fbc2a2ab1bf2b9d48823ecd1c7df43c956718e7f
sategie <>
2023-05-10 06:26:19 +0000

    Properly indent javascript code and remove unnecessary spacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,66 @@
 // Create Q&A array
 
-const questions = [
-    {
-      quest: "How many continents are there in the world?",
-      answers: ["5", "7", "8", "9"], correctIndex: 1
-    },
-  
-    {
-      quest: "What is the capital of Nigeria?",
-      answers: ["Lagos", "Ibadan", "Abuja", "Edo"], correctIndex: 2
-    },
-  
-    {
-      quest: "How many outfield players are there in football/soccer?",
-      answers: ["11", "10", "8", "9"], correctIndex: 1
-    },
-  
-    {
-      quest: "How many states are there in Germany?",
-      answers: ["16", "7", "8", "9"], correctIndex: 0
-    },
-  
-    {
-      quest: "How many milliseconds are there in a second?",
-      answers: ["24", "60", "100", "1000"], correctIndex: 3
-    },
+const questions = [{
+    quest: "How many continents are there in the world?",
+    answers: ["5", "7", "8", "9"],
+    correctIndex: 1
+},
 
-    {
-        quest: "Where is the largest desert in the world located?",
-        answers: ["Africa", "Europe", "Asia", "Antarctica"], correctIndex: 3
-      },
-    
-      {
-        quest: "Arachnophobia is the fear of what?",
-        answers: ["Dogs", "Cats", "Spiders", "Bees"], correctIndex: 2
-      },
-    
-      {
-        quest: "In what year did the Berlin wall fall?",
-        answers: [1989, 1999, 1945, 2000], correctIndex: 0
-      },
-    
-      {
-        quest: "Where did the rock band 'The Cranberries' originate from?",
-        answers: ["USA", "Ireland", "Denmark", "Sweden"], correctIndex: 1
-      },
-    
-      {
-        quest: "What is the national animal of Sweden?",
-        answers: ["Bear", "Tiger", "Lion", "Moose"], correctIndex: 3
-      }
+{
+    quest: "What is the capital of Nigeria?",
+    answers: ["Lagos", "Ibadan", "Abuja", "Edo"],
+    correctIndex: 2
+},
 
-  ];
+{
+    quest: "How many outfield players are there in football/soccer?",
+    answers: ["11", "10", "8", "9"],
+    correctIndex: 1
+},
+
+{
+    quest: "How many states are there in Germany?",
+    answers: ["16", "7", "8", "9"],
+    correctIndex: 0
+},
+
+{
+    quest: "How many milliseconds are there in a second?",
+    answers: ["24", "60", "100", "1000"],
+    correctIndex: 3
+},
+
+{
+    quest: "Where is the largest desert in the world located?",
+    answers: ["Africa", "Europe", "Asia", "Antarctica"],
+    correctIndex: 3
+},
+
+{
+    quest: "Arachnophobia is the fear of what?",
+    answers: ["Dogs", "Cats", "Spiders", "Bees"],
+    correctIndex: 2
+},
+
+{
+    quest: "In what year did the Berlin wall fall?",
+    answers: [1989, 1999, 1945, 2000],
+    correctIndex: 0
+},
+
+{
+    quest: "Where did the rock band 'The Cranberries' originate from?",
+    answers: ["USA", "Ireland", "Denmark", "Sweden"],
+    correctIndex: 1
+},
+
+{
+    quest: "What is the national animal of Sweden?",
+    answers: ["Bear", "Tiger", "Lion", "Moose"],
+    correctIndex: 3
+}
+
+];
 
 // Get the elements from HTML
 
@@ -77,51 +86,51 @@ startIcon.addEventListener("click", startGame);
 let shuffledQuestions = questions.sort(() => Math.random() - 0.5);
 
 /**
- * Starts the quiz game
- */
+* Starts the quiz game
+*/
 
 function startGame() {
-    startIcon.classList.add("hide");
-    quizContainer.classList.remove("hide");
-    userScore.classList.add("hide");
-  playAgain.classList.add("hide");
-  questionContain.classList.remove("hide");
-  questionNumber.classList.remove("hide");
-    showNextQuestion();
-  }
+startIcon.classList.add("hide");
+quizContainer.classList.remove("hide");
+userScore.classList.add("hide");
+playAgain.classList.add("hide");
+questionContain.classList.remove("hide");
+questionNumber.classList.remove("hide");
+showNextQuestion();
+}
 
 
 /**
- * Displays the next question in the game and calls the endGame function when the last question has been answered
- */
+* Displays the next question in the game and calls the endGame function when the last question has been answered
+*/
 
 function showNextQuestion() {
-    questionNumber.textContent = questionCount + " of " + questions.length;
-  if (currentQuestionIndex >= shuffledQuestions.length) {
+questionNumber.textContent = questionCount + " of " + questions.length;
+if (currentQuestionIndex >= shuffledQuestions.length) {
     endGame();
-  } else {
+} else {
     currentQuestion = shuffledQuestions[currentQuestionIndex];
     questionContent.textContent = currentQuestion.quest;
     for (let i = 0; i < answerContent.length; i++) {
-      answerContent[i].textContent = currentQuestion.answers[i];
+        answerContent[i].textContent = currentQuestion.answers[i];
     }
-  }
-  currentQuestionIndex++;
-  questionCount++;
+}
+currentQuestionIndex++;
+questionCount++;
 }
 
 /**
- * Checks if the provided answer is correct or not and alerts the user accordingly using the Sweet Alert API
- * 
- * @param {number} index - The user's selected answer index
- */
+* Checks if the provided answer is correct or not and alerts the user accordingly using the Sweet Alert API
+* 
+* @param {number} index - The user's selected answer index
+*/
 
 function checkAnswer(index) {
-    let rule = currentQuestionIndex === questions.length;
-    let correctAnswer = currentQuestion.answers[currentQuestion.correctIndex];
-    if (currentQuestion.correctIndex === index) {
-      score++;
-      Swal.fire({
+let rule = currentQuestionIndex === questions.length;
+let correctAnswer = currentQuestion.answers[currentQuestion.correctIndex];
+if (currentQuestion.correctIndex === index) {
+    score++;
+    Swal.fire({
         title: "Correct answer!",
         icon: "success",
         showCancelButton: false,
@@ -129,17 +138,17 @@ function checkAnswer(index) {
         allowEscapeKey: false,
         confirmButtonColor: "#3085d6",
         confirmButtonText: rule ? "Finish Quiz" : "Next Question"
-      }).then((result) => {
+    }).then((result) => {
         if (result.isConfirmed) {
-          if (rule) {
-            endGame();
-          } else {
-            showNextQuestion();
-          }
+            if (rule) {
+                endGame();
+            } else {
+                showNextQuestion();
+            }
         }
-      });
-    } else {
-      Swal.fire({
+    });
+} else {
+    Swal.fire({
         title: "Wrong Answer!",
         text: "The correct answer is " + correctAnswer,
         icon: "error",
@@ -148,49 +157,49 @@ function checkAnswer(index) {
         allowEscapeKey: false,
         confirmButtonColor: "#3085d6",
         confirmButtonText: rule ? "Finish Quiz" : "Next Question"
-      }).then((result) => {
+    }).then((result) => {
         if (result.isConfirmed) {
-          if (rule) {
-            endGame();
-          } else {
-            showNextQuestion();
-          }
+            if (rule) {
+                endGame();
+            } else {
+                showNextQuestion();
+            }
         }
-      });
-    }
-  }
+    });
+}
+}
 
 // Add Event listener to each answer button
 
 answerContent.forEach(function(element, index) {
-  element.addEventListener("click", function() {
+element.addEventListener("click", function() {
     checkAnswer(index);
-  });
+});
 });
 
 /**
- * Ends the quiz game, displays the final score to the user and gives the user the option to play again
- */
+* Ends the quiz game, displays the final score to the user and gives the user the option to play again
+*/
 function endGame() {
-  startIcon.classList.add("hide");
-  quizContainer.classList.add("hide");
-  userScore.classList.remove("hide");
-  userScore.textContent = "Your score is " + score + "/" + questions.length;
-  playAgain.classList.remove("hide");
-  questionContain.classList.add("hide");
-  questionNumber.classList.add("hide");
+startIcon.classList.add("hide");
+quizContainer.classList.add("hide");
+userScore.classList.remove("hide");
+userScore.textContent = "Your score is " + score + "/" + questions.length;
+playAgain.classList.remove("hide");
+questionContain.classList.add("hide");
+questionNumber.classList.add("hide");
 }
 
 // Add Event listener to the 'Play Again' button which resets the questions index and score, shuffles the questions array and calls the startGame function
 
 playAgain.addEventListener("click", function() {
-  currentQuestionIndex = 0;
-  score = 0;
-  questionCount = 1;
-  questionNumber.classList.remove("hide");
+currentQuestionIndex = 0;
+score = 0;
+questionCount = 1;
+questionNumber.classList.remove("hide");
 
-  // Use the slice method to create a new array with the current questions and then shuffle the array using the sort method
+// Use the slice method to create a new array with the current questions and then shuffle the array using the sort method
 
-  shuffledQuestions = questions.slice().sort(() => Math.random() - 0.5);
-  startGame();
+shuffledQuestions = questions.slice().sort(() => Math.random() - 0.5);
+startGame();
 });
